ae13fa780e6a78960cc60798b8272d98d55affce
Omar Faruk Sazib <ofsazib@gmail.com>
2019-07-31 02:42:03 +0600

    Added necessary configuration for the app

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+const App = () => (
+    <div>
+        <h1>Hello ReactJS!</h1>
+    </div>
+)
+
+ReactDOM.render(<App/>, document.getElementById('root'));
